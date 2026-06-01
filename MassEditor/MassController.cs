10326fab08daae0f2577a3711c49b5f4ff13196d
MazKnight <mazknight.online@gmail.com>
2026-06-01 13:13:04 -0700

    Added patches to accomodate for saving

--- MassEditor/MassController.cs
+++ MassEditor/MassController.cs
@@ -68,5 +68,62 @@ namespace MassEditor
 
 			return null;
 		}
+
+		#region Patchers
+		
+		[HarmonyDebug]
+		[HarmonyTranspiler]
+		[HarmonyPatch(typeof(CL_SaveManager), "SaveSessionToFile")]
+		private static IEnumerable<CodeInstruction> AddToJson(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			var matcher = new CodeMatcher(instructions, generator);
+		
+			matcher.MatchEndForward(
+				new CodeMatch(OpCodes.Ldarg_0),
+				new CodeMatch(OpCodes.Ldc_I4_1),
+				new CodeMatch(OpCodes.Call),
+				new CodeMatch(OpCodes.Stloc_0)
+			).ThrowIfInvalid("Cannot find proper string location");
+		
+			matcher.Advance(5);
+		
+			matcher.Insert(
+				new CodeInstruction(OpCodes.Ldloc_0),
+				CodeInstruction.Call(typeof(MassController), "InjectDataIntoJson"),
+				new CodeInstruction(OpCodes.Stloc_0)
+				);
+			
+			return matcher.InstructionEnumeration();
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch(typeof(CL_SaveManager.SaveState), "Save")]
+		private static void SaveDeathFloors(CL_SaveManager.SaveState __instance) 
+		{
+			GetMassController().SaveMassInstances(__instance);
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(CL_SaveManager), "LoadSession")]
+		private static void LoadSaveInstances()
+		{
+			string str = "";
+			if (CL_GameManager.IsHardmode())
+				str += "-hardmode";
+			string fileName = CL_GameManager.GetGamemodeName(false).ToLower() + str;
+
+			Debug.Log("yooohoo");
+			
+			GetMassController().CreateMassInstancesOnLoad(fileName);
+		}
+		
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(CL_SaveManager.SaveState), "LoadSave")]
+		private static void LoadSaves(CL_SaveManager.SaveState __instance)
+		{
+			GetMassController().LoadMassSaveStates(__instance);
+		}
+		
+		#endregion
 	}
 }
